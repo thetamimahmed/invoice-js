fd1ef7d98489f5fd8787f559f6b8bfd95f5b51da
thetamimahmed <wwwtamim000@gmail.com>
2022-04-25 17:32:47 +0600

    calculate prices and added error handler

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ document.getElementById("add-btn").addEventListener('click', function(){
     const itemPrice = document.getElementById('item-price');
     const itemQuantity = document.getElementById('item-quantity');
 
+    //error handeling / wrong inputs handle
+    if(itemName.value == '' || itemPrice.value < 0 || itemQuantity.value < 0){
+        alert('input valid information');
+        return;
+    }
+
+   
+
     //create tr and td in table
     const tr = newElement('tr');
     const td = newElement('td');
@@ -23,11 +31,19 @@ document.getElementById("add-btn").addEventListener('click', function(){
     const td3 = newElement('td');
     const td4 = newElement('td');
 
+    //set class on total td4
+    td4.classList.add('item-total')
+
     //set input values in  td
     td.innerText = itemName.value;
     td2.innerText = itemPrice.value;
     td3.innerText = itemQuantity.value; 
     td4.innerText = itemPrice.value * itemQuantity.value;
+
+    //clear input field
+    itemName.value = '';
+    itemPrice.value = '';
+    itemQuantity.value = '';
 
     //td append child in tr
     tr.appendChild(td);
@@ -37,9 +53,33 @@ document.getElementById("add-btn").addEventListener('click', function(){
 
     //tr append child in table body
     tableInfo.appendChild(tr);
+
+    calculatePrice();
 });
 
 //create element function
 function newElement(tag){
     return document.createElement(tag);
 };
+
+//calculate subtotal
+function calculateSubTotal(){
+    const itemTotal = document.getElementsByClassName('item-total');
+    let subTotal = 0;
+    for(let i = 0; i < itemTotal.length; i++){
+        const element = itemTotal[i];
+        const price = parseInt(element.innerText);
+        subTotal = subTotal + price;
+    }
+    return subTotal;
+}
+
+//calculate tax and grand total
+function calculatePrice(){
+    const subTotal = calculateSubTotal();
+    document.getElementById('subtotal').innerText = subTotal;
+    const tax = subTotal * .2;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total').innerText = subTotal + tax;
+    document.getElementById('total-amount').innerText = subTotal + tax;
+}
